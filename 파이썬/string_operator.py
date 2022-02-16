6bc99b0e44e1d091f62f9a2a4aca376e99eeb4a7
Koosungyoon <gusungyoon123@naver.com>
2022-02-17 00:30:56 +0900

    practic operator

---
+++ 파이썬/string_operator.py
@@ -0,0 +1,28 @@
+#\(이스케이프) 에 대해 알아보자
+
+print("name\told\tregion")
+print("Koo\t25\tHwasun")
+print("Lee\t23\tInchen")
+# \t 는 탭을 의미합니다
+
+
+print("문자 선택 연산자에 대해서 알아보자")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+
+print("안녕하세요"[-1])
+print("안녕하세요"[-2])
+print("안녕하세요"[-3])
+print("안녕하세요"[-4])
+print("안녕하세요"[-5])
+print()
+
+#문자열 범위 연산자 (슬라이싱)
+print("안녕하세요"[0:2])
+print("안녕하세요"[2:4])
+print("안녕하세요"[1:])
+print("안녕하세요"[:4])
