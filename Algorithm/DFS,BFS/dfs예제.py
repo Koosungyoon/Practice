e3edae2ea7ae8e5dd999df557a07e0e0f9353f62
Koosungyoon <gusungyoon123@naver.com>
2022-07-30 16:14:46 +0900

    DFS탐색 방법 맛보기

---
+++ Algorithm/DFS,BFS/dfs예제.py
@@ -0,0 +1,29 @@
+#DFS 메서드의 정의 
+from lib2to3.pgen2 import grammar
+
+
+def dfs(graph,v,visited):
+    #현재노드의 방문 처리
+    visited[v]=True
+    print(v,end=' ')
+    #현재노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+#각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph=[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+#각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited=[False]*9
+
+#정의된 DFS함수 호출
+dfs(graph,1,visited)
