58fc2dc25414ed3f5c04a7bc78700e973b14730d
Koosungyoon <gusungyoon123@naver.com>
2022-08-06 22:56:25 +0900

    퀵 정렬 구현

--- Algorithm/정렬/정렬구현.py
+++ Algorithm/정렬/정렬구현.py
@@ -2,6 +2,9 @@
 '''
 !!매번 가장 작은 것을 선택한다.!!
 '''
+from operator import le
+
+
 array=[7,5,9,0,3,1,6,2,4,8]
 
 for i in range(len(array)):
@@ -26,4 +29,36 @@ for i in range(1,len(array)):
             array[j],array[j-1]=array[j-1],array[j]
         else: #자기보다 작은 데이터를 만나면 그 위치에서 멈춤!
             break
+print(array)
+
+#퀵 정렬 구현
+'''
+기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾼다.
+'''
+
+array=[5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array,start,end):
+    if start>=end: #원소가 1개인 경우 종료
+        return 
+    pivot=start # 피벗은 첫 번쨰 원소
+    left=start+1
+    right=end
+    while left<=right:
+        #피벗보다 큰 데이터를 찾을 때까지 반복
+        while left<=end and array[left]<=array[pivot]:
+            left+=1
+        #피벗보다 작은 데이터를 찾을 떄까지 반복
+        while right>start and array[right]>=array[pivot]:
+            right-=1
+        if left>right: #엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right],array[pivot]=array[pivot],array[right]
+        else: #엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left],array[right]=array[right],array[left]
+        
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array,start,right-1)
+    quick_sort(array,right+1,end)
+
+quick_sort(array,0,len(array)-1)
 print(array)
