f26971343b337b1b543566534e902dfc6550250a
Koosungyoon <gusungyoon123@naver.com>
2022-08-23 18:30:50 +0900

    다이나믹 프로그래밍 문제 풀이

--- Algorithm/다이나믹 프로그래밍/test.py
+++ Algorithm/다이나믹 프로그래밍/test.py
@@ -18,4 +18,24 @@ for i in range(2,x+1):
     #5로 나누어 떨어지는 경우
     if i%5==0:
         d[i]=min(d[i],d[i//5]+1)
-    
+
+
+
+#효율적인 화폐 만들기
+
+import sys
+
+n,m=int(sys.stdin.readline().strip().split())
+
+arr=[]
+for _ in range(n):
+    arr.append(int(sys.stdin.readline().strip()))
+
+d=[10001]*(m+1)
+d[0]=0
+
+for i in range(n):
+    for j in range(arr[i],m+1):
+        if d[j-arr[i]] != 10001:
+            d[j]=min(d[j],d[j-arr[i]]+1)
+
