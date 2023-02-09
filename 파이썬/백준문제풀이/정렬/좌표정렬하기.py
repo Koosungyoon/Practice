7e7fdc84391c31d3a10869418ec7a7348ba7993b
Koosungyoon <gusungyoon123@naver.com>
2023-02-09 23:09:25 +0900

    백준 문제 풀이

---
+++ 파이썬/백준문제풀이/정렬/좌표정렬하기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+tmp = []
+
+for i in range(N):
+    x, y = map(int,input().split())
+    tmp.append((x, y))
+
+# print(tmp)
+# print(sorted(tmp))
+for i in sorted(tmp):
+    print(i[0],i[1])
+
+sorted(tmp,lambda )
