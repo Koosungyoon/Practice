052531b6d468030f690a3075814f9dc58ad9eec3
Koosungyoon <gusungyoon123@naver.com>
2023-02-15 23:57:48 +0900

    백준 문제 풀이

---
+++ 파이썬/백준문제풀이/정렬/단어정렬.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+res = []    
+for i in range(N):
+    tmp = []
+    tmp = input().strip()
+    tmp_len = len(tmp)
+    res.append([tmp,tmp_len])
+    # res_len.append(len(tmp))
+res1 = set(list(map(tuple,res)))
+res_sorted = sorted(res1)
+print(res1)
+print("\n")
+
+for a in sorted(res_sorted,key = lambda x: x[1]):
+    print(a[0])
