4f3f66ec95b4f94aedbcf6b2c4f31df37279e375
Koosungyoon <gusungyoon123@naver.com>
2023-02-24 01:52:10 +0900

    백준 문제 풀이

--- 파이썬/백준문제풀이/for문제/2444.py
+++ 파이썬/백준문제풀이/for문제/2444.py
@@ -4,9 +4,21 @@ input = sys.stdin.readline
 N = int(input().strip())
 
 for i in range(1,N+1):
-    print(" " * (N - i) + "*" * (i),end='')
-    print("*" *(i - 1) + " " * (N - i))
+    print(' ' * (N - i), end='')
+    print("*" *(i*2-1))
 
 for j in range(2,N+1):
-    print(" " * (j-1) + "*" * (N-j),end ='')
-    print("*" * (N-j+1) + " " * (j-1))
+    print(' ' * (j-1), end ='')
+    print("*" * ((N*2)-(j*2-1)))
+
+
+
+n = int(input())
+
+for i in range(n):
+    print(' ' * (n-i-1), end='')
+    print('*'*(i*2+1))
+
+for i in range(1,n):
+    print(' ' * i, end='')
+    print('*'*((n*2)-(i*2+1)))
