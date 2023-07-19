3e514a3ea2d9b0f48e6eb8c4cadfa14587b81166
Koosungyoon <gusungyoon123@naver.com>
2023-07-19 15:11:42 +0900

    Tree Traversal

---
+++ 파이썬/백준문제풀이/TreeProblem/1991.py
@@ -0,0 +1,35 @@
+#트리 순회 문제
+
+import sys
+LEFT = 0
+RIGHT = 1
+
+N = int(sys.stdin.readline().strip())
+tree = {}
+for _ in range(N):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root,end = '')
+        preorder(tree[root][LEFT])
+        preorder(tree[root][RIGHT])
+
+def innerorder(root):
+    if root != '.':
+        innerorder(tree[root][LEFT])
+        print(root,end = '')
+        innerorder(tree[root][RIGHT])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][LEFT])
+        postorder(tree[root][RIGHT])
+        print(root,end = '')
+
+preorder("A")
+print()
+innerorder("A")
+print()
+postorder("A")
