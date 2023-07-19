472a69ea1cb1a1cc0d45b9055b51bcfec0560046
Koosungyoon <gusungyoon123@naver.com>
2023-07-19 13:41:28 +0900

    DP 풀이 or 분할정복 제곱법

--- 파이썬/백준문제풀이/단기간 성장/11401.py
+++ 파이썬/백준문제풀이/단기간 성장/11401.py
@@ -1,4 +1,6 @@
-#DP 문제 풀이인줄 알고 접근하려 했으나 런타임 오류로 -> 분할 정복 제곱 기법을 사용함! -> 새로 배운 개념 
+# 문제 : 이항 계수 구하는 방법! 
+
+# 최종 -> 분할 정복 제곱 기법을 사용함! -> 새로 배운 개념 
 import sys
 
 a = 1000000007
@@ -23,4 +25,40 @@ def DevideConQuerSquare(C, n):
 
 res = fact(N) * DevideConQuerSquare((fact(N-K) * fact(K)), a-2) % a
 print(res)
+
+################################
+#DP 사용하여 문제 풀이 -> 메모리 초과
+import sys
+
+a = 1000000007
+
+def init(n, K):
+    res = [[a for _ in range(n)] for _ in range(K + 1)]
+    for i in range(n):
+        res[0][i] = 1
+        res[1][i] = i + 1
+        if i < K:
+            res[i + 1][i] = 1
+    return res
+
+def DP(N, K, res):
+    if res[K][N - 1] == a:
+        res[K][N - 1] = DP(N - 1, K - 1, res) + DP(N - 1, K, res)
+    return res[K][N - 1]
+
+def calculate_combinations(N, K):
+    res = init(N, K)
+    result = DP(N, K, res)
+    return result
+
+# Read input from command line
+N, K = map(int, sys.stdin.readline().strip().split())
+
+result = calculate_combinations(N, K)
+print(result % a)
+
+
+
+
+
 
