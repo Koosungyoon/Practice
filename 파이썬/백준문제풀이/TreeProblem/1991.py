20bed94019d84691a686b8671f46f3d2ec81c979
Koosungyoon <gusungyoon123@naver.com>
2023-07-19 15:13:43 +0900

    Tree Traversal

--- 파이썬/백준문제풀이/TreeProblem/1991.py
+++ 파이썬/백준문제풀이/TreeProblem/1991.py
@@ -10,18 +10,21 @@ for _ in range(N):
     root, left, right = sys.stdin.readline().strip().split()
     tree[root] = [left, right]
 
-def preorder(root):
+#root - left - right
+def preorder(root): 
     if root != '.':
         print(root,end = '')
         preorder(tree[root][LEFT])
         preorder(tree[root][RIGHT])
 
+#left - root - right
 def innerorder(root):
     if root != '.':
         innerorder(tree[root][LEFT])
         print(root,end = '')
         innerorder(tree[root][RIGHT])
 
+#letf - right - root
 def postorder(root):
     if root != '.':
         postorder(tree[root][LEFT])
